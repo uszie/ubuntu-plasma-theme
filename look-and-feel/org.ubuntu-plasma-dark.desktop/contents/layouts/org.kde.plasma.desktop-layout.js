49b9f3fc9751c2469bf099138c4d81e5e6de6054
usarinheininga@hotmail.com <usarinheininga@hotmail.com>
2020-11-07 22:08:04 +0100

    Add latte-dock template. Set lockscreen wallpaper as well.

--- look-and-feel/org.ubuntu-plasma-dark.desktop/contents/layouts/org.kde.plasma.desktop-layout.js
+++ look-and-feel/org.ubuntu-plasma-dark.desktop/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -1,7 +1,20 @@
-var config = ConfigFile('ksplashrc');
-config.group = 'KSplash';
-config.writeEntry('Engine', 'None');
-config.writeEntry('Theme', 'None');
+var ksplashrcConfig = ConfigFile('ksplashrc');
+ksplashrcConfig.group = 'KSplash';
+ksplashrcConfig.writeEntry('Engine', 'None');
+ksplashrcConfig.writeEntry('Theme', 'None');
+
+var kscreenlockerrcConfig = ConfigFile('kscreenlockerrc');
+kscreenlockerrcConfig.group = 'Greeter';
+
+var wallpaperConfig = ConfigFile(kscreenlockerrcConfig, 'Wallpaper');
+wallpaperConfig.group = 'Wallpaper';
+
+var imageConfig = ConfigFile(wallpaperConfig, 'org.kde.image');
+imageConfig.group = 'org.kde.image';
+
+var generalConfig = ConfigFile(imageConfig, 'General');
+generalConfig.group = 'General';
+generalConfig.writeEntry('Image', '/usr/share/wallpapers/Ubuntu');
 
 var desktopsArray = desktopsForActivity(currentActivity());
 for( var j = 0; j < desktopsArray.length; j++) {
